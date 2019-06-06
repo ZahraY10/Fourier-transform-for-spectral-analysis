a8549109d970f51324899f1900b7c686cee15005
ZahraY10 <zr.yousefi10@gmail.com>
2019-06-07 01:07:16 +0430

    a matlab script for ploting the power spectrum of a signal

--- plot_power_spectrum.m
+++ plot_power_spectrum.m
@@ -1,7 +1,7 @@
 ## Author: zahra <zahra@zahra-Lenovo-ideapad-500-15ISK>
 ## Created: 2019-06-06
 
-[x, fs] = audioread('v0.wav'); ##data and sampling frequency
+[x, fs] = audioread('voices/v0.wav'); ##data and sampling frequency
 y = fft(x); ##fast fourier transfer on signal data
 n = length(x);
 f = (0:n-1)*(fs/n); ##fs/n: frequency increment --> frequency range
