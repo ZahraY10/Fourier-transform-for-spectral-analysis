7e6b2790862d8d2bf731b902109d6d0b78098008
ZahraY10 <zr.yousefi10@gmail.com>
2019-06-13 23:27:37 +0430

    added renaming of the files to specify gender of the speaker

--- labeling.m
+++ labeling.m
@@ -1,13 +1,28 @@
+## Author: zahra <zahra@zahra-Lenovo-ideapad-500-15ISK>
+## Created: 2019-06-06
+
 function [retval] = labeling (folder)
 audio_files=dir(fullfile(folder,'*.wav'))
-eps = 25;
-for k=3:numel(audio_files)
+eps = 30; ##Error in peak frequency
+for k=1:numel(audio_files)
   file_address = strcat(folder, '/', audio_files(k).name);
   tmp = peak_frq(file_address);
-  if(tmp > 122 - eps && tmp < 122 + eps) 
+  printf("%s peak = %d\n", audio_files(k).name, tmp(1,1)); 
+  if(tmp > 122 - eps && tmp < 122 + eps)
+    f = audio_files(k).name;
+    rf = strcat(f,'_M') ;
+    movefile(fullfile(folder, audio_files(k).name), fullfile(folder, rf));     
     printf("%s peak = %d therefore male voice\n", audio_files(k).name, tmp(1,1)); 
   elseif(tmp > 212 - eps && tmp < 212 + eps) 
+    f = audio_files(k).name;
+    rf = strcat(f,'_F') ;
+    movefile(fullfile(folder, audio_files(k).name), fullfile(folder, rf));     
     printf("%s peak = %d therefore female voice\n", audio_files(k).name, tmp(1,1));
+  else
+    f = audio_files(k).name;
+    rf = strcat(f,'_U') ;
+    movefile(fullfile(folder, audio_files(k).name), fullfile(folder, rf));     
+    printf("%s peak = %d therefore unknown gender\n", audio_files(k).name, tmp(1,1));
   end 
 end
 endfunction
